36953cecb0474d67ef86b82c8824228c5deda832
chensl <chensl@DESKTOP-LLC41RJ>
2020-06-29 15:00:40 +0800

    增加可以用args参数来实验sdk

--- src/main/java/com/zifisense/zetag/mq/api/Main.java
+++ src/main/java/com/zifisense/zetag/mq/api/Main.java
@@ -3,18 +3,28 @@ import com.zifisense.zetag.mq.api.imp.ClientFactory;
 import com.zifisense.zetag.mq.api.imp.ClientType;
 public class Main {
 	public static void main(String[] args) {
+		
+		
 		String apiKey;
 		String apiSecret;
 		String companyCode;
 		String topic;
-		//apiKey，当前版本一个企业只有一个apiKey，就是企业的企业编号，后续版本一个企业可能有多个apiKey
-		apiKey = "cf86bc5d68d54932a854a62626c36823";
-		//相当于企业秘钥，后续版本可能也会有多个
-		apiSecret  = "3c109b9463504e02bd957cd935f57cae";
-		//企业编号
-		companyCode  = "cf86bc5d68d54932a854a62626c36823";
-		//描述需要获取哪些数据
-		topic = "zetag-heartbeatall";
+		
+		if(args.length==4) {
+			apiKey = args[0];
+			apiSecret = args[1];
+			companyCode = args[2];
+			topic = args[3];
+		}else {
+			//apiKey，当前版本一个企业只有一个apiKey，就是企业的企业编号，后续版本一个企业可能有多个apiKey
+			apiKey = "cf86bc5d68d54932a854a62626c36823";
+			//相当于企业秘钥，后续版本可能也会有多个
+			apiSecret  = "3c109b9463504e02bd957cd935f57cae";
+			//企业编号
+			companyCode  = "cf86bc5d68d54932a854a62626c36823";
+			//描述需要获取哪些数据
+			topic = "zetag-heartbeatall";
+		}
 		//建立客户端
 		ZiFiClient c = ClientFactory.createClient(ClientType.KAFKA,RegionEnum.CN, apiKey, apiSecret, companyCode);
 		//订阅topic
