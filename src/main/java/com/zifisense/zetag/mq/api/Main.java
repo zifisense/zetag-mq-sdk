20ef92485f596eb44ddf253ad38c4cdb4a41bbf7
chensl <chensl@DESKTOP-LLC41RJ>
2020-08-13 15:54:02 +0800

    修改bug

--- src/main/java/com/zifisense/zetag/mq/api/Main.java
+++ src/main/java/com/zifisense/zetag/mq/api/Main.java
@@ -9,11 +9,11 @@ public class Main {
 		String companyCode;
 		String topic;
 		//apiKey，当前版本一个企业只有一个apiKey，就是企业的企业编号，后续版本一个企业可能有多个apiKey
-		apiKey = "0d48f33151b34120a7a25d1c64387c3c";
+		apiKey = "559daf1b28dc4b83a057bf01395db09d";
 		//相当于企业秘钥，后续版本可能也会有多个
-		apiSecret  = "227bdb21673241048c7fc86a9f72d69b";
+		apiSecret  = "2ef78227f5da4f5a8e8da7077d417d42";
 		//企业编号
-		companyCode  = "0d48f33151b34120a7a25d1c64387c3c";
+		companyCode  = "559daf1b28dc4b83a057bf01395db09d";
 		//描述需要获取哪些数据
 		topic = "zetag-heartbeatall";
 		//建立客户端
@@ -23,7 +23,7 @@ public class Main {
 		while (true) {
 			//循环处理收到的数据
 			c.poll().forEach(message->{
-				System.out.printf("id = %d, value = %s%n", message.getMessageId(), message.getBody());
+				System.out.printf("id = %s, value = %s%n", message.getMessageId(), message.getBody());
 			});
 			//commit之后才能继续消费下一批次数据
 			c.commit();
