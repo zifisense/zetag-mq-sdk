e8ccc21f6454150b321a72bc8b9f67a3428162a8
chensl <chensl@DESKTOP-LLC41RJ>
2020-09-18 15:21:04 +0800

    更新示例topic名称

--- src/main/java/com/zifisense/zetag/mq/api/Main.java
+++ src/main/java/com/zifisense/zetag/mq/api/Main.java
@@ -9,13 +9,13 @@ public class Main {
 		String companyCode;
 		String topic;
 		//apiKey，当前版本一个企业只有一个apiKey，就是企业的企业编号，后续版本一个企业可能有多个apiKey
-		apiKey = "559daf1b28dc4b83a057bf01395db09d";
+		apiKey = "";
 		//相当于企业秘钥，后续版本可能也会有多个
-		apiSecret  = "2ef78227f5da4f5a8e8da7077d417d42";
+		apiSecret  = "";
 		//企业编号
-		companyCode  = "559daf1b28dc4b83a057bf01395db09d";
+		companyCode  = "";
 		//描述需要获取哪些数据
-		topic = "zetag-heartbeatall";
+		topic = "zetag-heartbeat-all";
 		//建立客户端
 		ZiFiClient c = ClientFactory.createClient(ClientType.KAFKA,RegionEnum.CN, apiKey, apiSecret, companyCode);
 		//订阅topic
